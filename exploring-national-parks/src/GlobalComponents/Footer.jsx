875a4f621599f8d5078b48fd167a49625af8dcf4
tug94192 <tug94192@temple.edu>
2024-03-19 16:05:47 -0400

    Embedded a looping youtube video at the bottom of the page

--- exploring-national-parks/src/GlobalComponents/Footer.jsx
+++ exploring-national-parks/src/GlobalComponents/Footer.jsx
@@ -24,8 +24,8 @@ const Footer = () => {
       <a class="twitter-timeline" data-height="200" data-theme="dark" href="https://twitter.com/TempleUniv?ref_src=twsrc%5Etfw">Tweets by TempleUniv</a> 
       </div>
 
-      <div class = 'youtube-video'>
-      <iframe src="https://www.youtube.com/embed/PKtnafFtfEo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"><small>Powered by <a href="https://embed.tube/embed-code-generator/youtube/">youtube embed video</a> generator</small></iframe>
+      <div class = 'youtube-video-container'>
+        <iframe class='youtube-video' width="100%" height="100%" src="https://www.youtube.com/embed/diy_b2eoDN8?autoplay=1&loop=1&mute=1&playlist=diy_b2eoDN8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"><small>Powered by <a href="https://embed.tube/embed-code-generator/youtube/">youtube embed video</a> generator</small></iframe>
       </div>
     </div>
     
